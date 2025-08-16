16624ca4a409016277a1f93d1f987d8c56e95c0d
Magnus Wållberg <magnus@tagpad.net>
2025-08-16 03:15:57 +0200

    Support DumbAware mode

--- src/main/java/net/tagpad/cmakemux/CMakeMuxToolWindowFactory.java
+++ src/main/java/net/tagpad/cmakemux/CMakeMuxToolWindowFactory.java
@@ -2,6 +2,7 @@ package net.tagpad.cmakemux;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -10,16 +11,26 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
-public final class CMakeMuxToolWindowFactory implements ToolWindowFactory {
+public final class CMakeMuxToolWindowFactory implements ToolWindowFactory, DumbAware {
 
     public static final String TOOL_WINDOW_ID = "CMake Mux";
 
     @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        // Keep the stripe button visible even during indexing
+        return true;
+    }
+
+    @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        // Ensure stripe button isn’t hidden in dumb mode
+        toolWindow.setAvailable(true, null);
+
         CMakeMuxPanel panel = new CMakeMuxPanel(project);
         Content content = ContentFactory.getInstance().createContent(panel, "", false);
-        content.setDisposer(panel); // ensures message-bus connection is disposed on unload
+        content.setDisposer(panel);
         toolWindow.getContentManager().addContent(content);
+
         toolWindow.setAnchor(ToolWindowAnchor.LEFT, null);
         toolWindow.setSplitMode(true, null);
     }
