c5d484a5e32d3ebc847e407a601dca560f4cabc8
Magnus Wållberg <magnus@tagpad.net>
2025-08-18 19:31:29 +0200

    Updated documentation and added GItHub build plugin workflow.

--- src/main/java/net/tagpad/cmakemux/CMakeMuxPopupAction.java
+++ src/main/java/net/tagpad/cmakemux/CMakeMuxPopupAction.java
@@ -7,7 +7,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.io.FileUtil;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,7 @@ public class CMakeMuxPopupAction extends AnAction implements DumbAware {
         int max = Math.min(9, entries.size());
         DefaultActionGroup group = new DefaultActionGroup();
 
+        String activePath = CMakeMuxSelectionService.getInstance(project).getActivePath();
         for (int i = 0; i < max; i++) {
             final int index = i;
             CMakeMuxEntry entry = entries.get(i);
@@ -34,7 +37,10 @@ public class CMakeMuxPopupAction extends AnAction implements DumbAware {
                     ? entry.getNickname()
                     : entry.getPath();
 
-            group.add(new AnAction(title, entry.getPath(), AllIcons.Actions.Execute) {
+            boolean isActive = activePath != null && FileUtil.pathsEqual(activePath, entry.getPath());
+            Icon icon = isActive ? AllIcons.Debugger.NextStatement : AllIcons.Actions.ProjectDirectory;
+
+            group.add(new AnAction(title, entry.getPath(), icon) {
                 @Override
                 public void actionPerformed(@NotNull AnActionEvent ignored) {
                     CMakeMuxLoader.loadEntry(project, entries.get(index));
